06b3e844617abe87023a4abfedf3bbe127aea833
Rafaela <rafaela.lima@indt.org.br>
2018-07-03 12:34:51 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+
+
+import { AuthorsService } from './authors.service';
+import { HttpClientModule } from "@angular/common/http";
+import { TableComponent } from './table/table.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthorsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/authors.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { Observable } from "rxjs";
+import { catchError, map, tap } from "rxjs/operators";
+
+@Injectable()
+export class AuthorsService {
+
+  private url = 'https://bibliapp.herokuapp.com/api';
+  private httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+
+  constructor(private http: HttpClient) { }
+
+  private formatErrors(error: any) {
+    return Observable.throw(error);
+  }
+
+  getAuthor(): Observable<any>{
+    return this.http.get(this.url+'/authors').catch(this.formatErrors);
+  }
+
+  getAuthorById(id: number): Observable<any>{
+    return this.http.get(this.url+'/authors/'+id).catch(this.formatErrors);
+  }
+
+  updateAuthor(firstName: string, lastName: string): Observable<any> {
+    const author = {
+      'firstName': firstName,
+      'lastName': lastName
+    };
+
+    return this.http.put(this.url+'/authors', author, this.httpOptions).catch(this.formatErrors);
+  }
+
+  addAuthor(firstName: string, lastName: string): Observable<any> {
+    const author = {
+      'firstName': firstName,
+      'lastName': lastName
+    };
+
+    return this.http.post(this.url+'/authors', author, this.httpOptions).catch(this.formatErrors);
+  }
+
+  deleteAuthor (id: number): Observable<any> {
+    return this.http.delete(this.url+'/authors/'+id, this.httpOptions).catch(this.formatErrors);
+  }
+
+  searchAuthor(value: string): Observable<any> {
+    return this.http.get(this.url+'/authors?filter='+value).catch(this.formatErrors);
+  }
+
+}
